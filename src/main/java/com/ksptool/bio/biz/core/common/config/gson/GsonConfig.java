5d5c1739f961aebfdbb28dbb99930a061c88fa81
KspTooi <ksptooi@outlook.com>
2026-05-02 01:57:26 +0800

    fix: 修复全局日志使用GSON在实例化、反实例化LDT、LD时出现错误

---
+++ src/main/java/com/ksptool/bio/biz/core/common/config/gson/GsonConfig.java
@@ -0,0 +1,27 @@
+package com.ksptool.bio.biz.core.common.config.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.ksptool.bio.commons.config.LocalDateTimeAdapter;
+import com.ksptool.bio.commons.config.OffsetDateTimeAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+
+@Configuration
+public class GsonConfig {
+
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .registerTypeAdapter(LocalDate.class, new LocalDateAdapter())
+                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                .registerTypeAdapter(OffsetDateTime.class, new OffsetDateTimeAdapter())
+                .create();
+    }
+
+
+}
