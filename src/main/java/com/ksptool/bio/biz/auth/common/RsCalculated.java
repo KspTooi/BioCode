f016739f0d4db944a96e12a80c803ad67afdb65f
KspTooi <ksptooi@outlook.com>
2026-05-02 02:03:51 +0800

    feat: 新增RsCalculator和RsCalculated类以支持行级数据权限计算，提供用户RS等级及允许访问的组织ID集合

---
+++ src/main/java/com/ksptool/bio/biz/auth/common/RsCalculated.java
@@ -0,0 +1,26 @@
+package com.ksptool.bio.biz.auth.common;
+
+import java.util.Set;
+
+/**
+ * 行级数据权限计算结果
+ * 用于存储用户行级数据权限计算结果
+ * 
+ *
+ * @author KspTool
+ * @since 2026-04-28
+ */
+public record RsCalculated(int rsMax, Set<Long> allowOrgIds) {
+
+    /**
+     * 构造函数
+     * 
+     * @param rsMax 数据权限最大等级
+     * @param allowOrgIds 允许访问的组织IDS(可部门可公司)
+     * @return RsCalculated 行级数据权限计算结果
+     */
+    public static RsCalculated of(int rsMax, Set<Long> allowOrgIds) {
+        return new RsCalculated(rsMax, allowOrgIds);
+    }
+}
+
