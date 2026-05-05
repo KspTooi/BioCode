8c8d98aac15e904ad081952b4aaec1960c543b19
KspTooi <ksptooi@outlook.com>
2026-05-05 08:13:27 +0800

    feat: 添加查询菜单包列表和根据编码统计菜单包数量的方法到 PackRepository

--- src/main/java/com/ksptool/bio/biz/core/repository/PackRepository.java
+++ src/main/java/com/ksptool/bio/biz/core/repository/PackRepository.java
@@ -11,13 +11,31 @@ import org.springframework.data.repository.query.Param;
 @Repository
 public interface PackRepository extends JpaRepository<PackPo, Long>{
 
+    /**
+     * 查询菜单包列表
+     *
+     * @param po       查询条件
+     * @param pageable 分页条件
+     * @return 菜单包列表
+     */
     @Query("""
-    SELECT u FROM PackPo u
-    WHERE
-    (:#{#po.name} IS NULL OR u.name LIKE CONCAT('%', :#{#po.name}, '%'))
-    AND (:#{#po.code} IS NULL OR u.code LIKE CONCAT('%', :#{#po.code}, '%'))
-    AND (:#{#po.status} IS NULL OR u.status = :#{#po.status} )
-    ORDER BY u.createTime DESC
-    """)
+            SELECT u FROM PackPo u
+            WHERE
+            (:#{#po.name} IS NULL OR u.name LIKE CONCAT('%', :#{#po.name}, '%'))
+            AND (:#{#po.code} IS NULL OR u.code LIKE CONCAT('%', :#{#po.code}, '%'))
+            AND (:#{#po.status} IS NULL OR u.status = :#{#po.status} )
+            ORDER BY u.createTime DESC
+            """)
     Page<PackPo> getPackList(@Param("po") PackPo po, Pageable pageable);
+
+    /**
+     * 根据编码统计菜单包数量
+     *
+     * @param code 编码
+     * @return 菜单包数量
+     */
+    @Query("""
+            SELECT COUNT(u) FROM PackPo u WHERE u.code = :code
+            """)
+    Integer countByCode(@Param("code") String code);
 }
