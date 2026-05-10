bca5537011458583bbadb5fb324de0d44c3e84bf
KspTooi <ksptooi@outlook.com>
2026-05-11 02:59:05 +0800

    feat: 主版本升级为1.6Y70

--- src/main/java/com/ksptool/bio/BioRunner.java
+++ src/main/java/com/ksptool/bio/BioRunner.java
@@ -33,7 +33,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class BioRunner {
 
     //应用版本号
-    private static final AppVersion appVersion = AppVersion.of("1.6X68");
+    private static final AppVersion appVersion = AppVersion.of("1.6Y70");
 
     @Autowired
     private RelayServerService relayServerService;
