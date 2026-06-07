cf3af4542c7f9e77821dd94a2f6e2adc57aa92b6
KspTooi <ksptooi@outlook.com>
2026-06-08 00:00:00 +0800

    feat: 添加LaunchQfProcessDto和GetProcNodeDefineDto类，增强流程启动和节点配置功能；更新LaunchProcDto以支持启动成员参数列表

--- src/main/java/com/ksptool/bio/biz/qf/model/qfmodeldeployrcd/dto/LaunchQfProcessDto.java
+++ src/main/java/com/ksptool/bio/biz/qf/model/qfmodeldeployrcd/dto/LaunchQfProcessDto.java
@@ -1,10 +1,5 @@
 package com.ksptool.bio.biz.qf.model.qfmodeldeployrcd.dto;
 
-import java.util.Set;
-
-import com.ksptool.bio.biz.qf.commons.LaunchParam;
-
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -12,17 +7,12 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class LaunchProcDto {
+public class LaunchQfProcessDto {
 
     @NotBlank(message = "模型编码不能为空")
     private String code;
 
     @NotNull(message = "业务数据ID不能为空")
     private Long dataId;
-
-    @NotNull(message = "启动成员参数列表不能为空")
-    @Schema(description = "启动成员参数列表")
-    private Set<LaunchParam.LaunchMemberParam> members;
-
 
 }
