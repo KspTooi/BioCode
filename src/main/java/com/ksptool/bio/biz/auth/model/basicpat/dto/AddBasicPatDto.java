c6679076ed479202c3453c2c111be54d4cf2f9eb
KspTooi <ksptooi@outlook.com>
2026-06-13 03:24:43 +0800

    feat: 实现AddBasicPatDto类的自定义验证功能
    - 将AddBasicPatDto类实现DtoCustomValidator接口，添加过期时间的校验逻辑，确保过期时间不能为过去的时间。
    - 更新相关注释以增强代码可读性和维护性。

--- src/main/java/com/ksptool/bio/biz/auth/model/basicpat/dto/AddBasicPatDto.java
+++ src/main/java/com/ksptool/bio/biz/auth/model/basicpat/dto/AddBasicPatDto.java
@@ -1,8 +1,9 @@
 package com.ksptool.bio.biz.auth.model.basicpat.dto;
 
+import com.ksptool.bio.biz.core.common.aop.DtoCustomValidator;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-public class AddBasicPatDto {
+public class AddBasicPatDto implements DtoCustomValidator {
 
     @NotNull(message = "PAT名称不能为空")
     @Size(max = 40, message = "PAT名称长度不能超过40")
@@ -20,4 +21,16 @@ public class AddBasicPatDto {
     @Schema(description = "过期时间")
     private LocalDateTime expire;
 
+    /**
+     * 校验过期时间不能是过去
+     *
+     * @return 验证结果 验证通过返回null 否则返回错误信息
+     */
+    @Override
+    public String validate() {
+        if (expire != null && expire.isBefore(LocalDateTime.now())) {
+            return "过期时间不能是过去";
+        }
+        return null;
+    }
 }
