f701788d5739f06c33277eaca89ed4cb98cc4932
KspTooi <ksptooi@outlook.com>
2026-06-21 03:20:27 +0800

    fix: 删除测试生成代码

--- src/main/java/com/ksptool/bio/biz/assembly1/mapper/OpRcdMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ksptool.bio.biz.assembly1.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-@Mapper
-public interface OpRcdMapper {
-
-}
