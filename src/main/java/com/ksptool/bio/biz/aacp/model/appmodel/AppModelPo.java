0249f49d34f25401b0e77c122e0feabf61e16a59
Colossus-Agt <bobksptooi@gmail.com>
2026-06-21 04:28:44 +0800

    feat: 更新AAM表 添加正确的复合主键

--- src/main/java/com/ksptool/bio/biz/aacp/model/appmodel/AppModelPo.java
+++ src/main/java/com/ksptool/bio/biz/aacp/model/appmodel/AppModelPo.java
@@ -1,45 +1,32 @@
 package com.ksptool.bio.biz.aacp.model.appmodel;
 
-import com.ksptool.assembly.entity.exception.AuthException;
-import com.ksptool.bio.biz.core.common.jpa.SnowflakeIdGenerated;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
-import java.time.LocalDateTime;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import com.ksptool.assembly.entity.exception.AuthException;
-import com.ksptool.bio.biz.auth.service.SessionService;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
+import lombok.*;
+
+import java.io.Serializable;
 
 @Getter
 @Setter
+@IdClass(AppModelPo.Pk.class)
 @Entity
-@Table(name = "aacp_app_model")
-@EntityListeners(AuditingEntityListener.class)
+@Table(name = "aacp_app_model", comment = "AAM表")
 public class AppModelPo {
 
     @Id
-    @SnowflakeIdGenerated
     @Column(name = "app_id", nullable = false, comment = "AID")
     private Long appId;
 
     @Id
-    @SnowflakeIdGenerated
     @Column(name = "model_id", nullable = false, comment = "MID")
     private Long modelId;
 
-
-    @PrePersist
-    private void onCreate() throws AuthException {
-    }
-
-    @PreUpdate
-    private void onUpdate() throws AuthException {
-
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    public static class Pk implements Serializable {
+        private Long appId;
+        private Long modelId;
     }
 }
