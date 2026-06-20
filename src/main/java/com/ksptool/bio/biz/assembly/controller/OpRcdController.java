9e7aeaa9d8b501643660f5f1878d80992a480d67
Colossus-Agt <bobksptooi@gmail.com>
2026-06-21 02:43:02 +0800

    refactor: 重构OpRcdController和OpRcdService，删除不再使用的DTO和VO类
    - 更新OpRcdController中的导入顺序，确保一致性。
    - 删除不再使用的AddOpRcdDto、EditOpRcdDto、GetOpRcdListDto、GetOpRcdDetailsVo和GetOpRcdListVo类，简化代码结构。
    - 优化OpRcdService中的方法，确保代码风格一致性。

--- src/main/java/com/ksptool/bio/biz/assembly/controller/OpRcdController.java
+++ src/main/java/com/ksptool/bio/biz/assembly/controller/OpRcdController.java
@@ -3,22 +3,23 @@ package com.ksptool.bio.biz.assembly.controller;
 import com.ksptool.assembly.entity.web.CommonIdDto;
 import com.ksptool.assembly.entity.web.PageResult;
 import com.ksptool.assembly.entity.web.Result;
-
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import jakarta.validation.Valid;
-import org.springframework.security.access.prepost.PreAuthorize;
-import com.ksptool.bio.commons.annotation.PrintLog;
-
-import com.ksptool.bio.biz.assembly.service.OpRcdService;
 import com.ksptool.bio.biz.assembly.model.oprcd.dto.AddOpRcdDto;
 import com.ksptool.bio.biz.assembly.model.oprcd.dto.EditOpRcdDto;
 import com.ksptool.bio.biz.assembly.model.oprcd.dto.GetOpRcdListDto;
-import com.ksptool.bio.biz.assembly.model.oprcd.vo.GetOpRcdListVo;
 import com.ksptool.bio.biz.assembly.model.oprcd.vo.GetOpRcdDetailsVo;
+import com.ksptool.bio.biz.assembly.model.oprcd.vo.GetOpRcdListVo;
+import com.ksptool.bio.biz.assembly.service.OpRcdService;
+import com.ksptool.bio.commons.annotation.PrintLog;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @PrintLog
 @RestController
@@ -32,42 +33,42 @@ public class OpRcdController {
 
     @PreAuthorize("@auth.hasCode('assembly:op:rcd:view')")
     @PostMapping("/getOpRcdList")
-    @Operation(summary ="查询输出方案执行记录列表")
-    public PageResult<GetOpRcdListVo> getOpRcdList(@RequestBody @Valid GetOpRcdListDto dto) throws Exception{
+    @Operation(summary = "查询输出方案执行记录列表")
+    public PageResult<GetOpRcdListVo> getOpRcdList(@RequestBody @Valid GetOpRcdListDto dto) throws Exception {
         return opRcdService.getOpRcdList(dto);
     }
 
     @PreAuthorize("@auth.hasCode('assembly:op:rcd:add')")
-    @Operation(summary ="新增输出方案执行记录")
+    @Operation(summary = "新增输出方案执行记录")
     @PostMapping("/addOpRcd")
-    public Result<String> addOpRcd(@RequestBody @Valid AddOpRcdDto dto) throws Exception{
-		opRcdService.addOpRcd(dto);
+    public Result<String> addOpRcd(@RequestBody @Valid AddOpRcdDto dto) throws Exception {
+        opRcdService.addOpRcd(dto);
         return Result.success("新增成功");
     }
 
     @PreAuthorize("@auth.hasCode('assembly:op:rcd:edit')")
-    @Operation(summary ="编辑输出方案执行记录")
+    @Operation(summary = "编辑输出方案执行记录")
     @PostMapping("/editOpRcd")
-    public Result<String> editOpRcd(@RequestBody @Valid EditOpRcdDto dto) throws Exception{
-		opRcdService.editOpRcd(dto);
+    public Result<String> editOpRcd(@RequestBody @Valid EditOpRcdDto dto) throws Exception {
+        opRcdService.editOpRcd(dto);
         return Result.success("修改成功");
     }
 
     @PreAuthorize("@auth.hasCode('assembly:op:rcd:view')")
-    @Operation(summary ="查询输出方案执行记录详情")
+    @Operation(summary = "查询输出方案执行记录详情")
     @PostMapping("/getOpRcdDetails")
-    public Result<GetOpRcdDetailsVo> getOpRcdDetails(@RequestBody @Valid CommonIdDto dto) throws Exception{
+    public Result<GetOpRcdDetailsVo> getOpRcdDetails(@RequestBody @Valid CommonIdDto dto) throws Exception {
         GetOpRcdDetailsVo details = opRcdService.getOpRcdDetails(dto);
-        if(details == null){
+        if (details == null) {
             return Result.error("无数据");
         }
         return Result.success(details);
     }
 
     @PreAuthorize("@auth.hasCode('assembly:op:rcd:remove')")
-    @Operation(summary ="删除输出方案执行记录")
+    @Operation(summary = "删除输出方案执行记录")
     @PostMapping("/removeOpRcd")
-    public Result<String> removeOpRcd(@RequestBody @Valid CommonIdDto dto) throws Exception{
+    public Result<String> removeOpRcd(@RequestBody @Valid CommonIdDto dto) throws Exception {
         opRcdService.removeOpRcd(dto);
         return Result.success("操作成功");
     }
